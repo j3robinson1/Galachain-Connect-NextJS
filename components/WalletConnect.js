0b7e25e045619f1f11ff6f843446769f5ce48e76
Jesse Robinson <j3robinson1@gmail.com>
2025-01-04 10:03:01 -0500

    fix

--- components/WalletConnect.js
+++ components/WalletConnect.js
@@ -15,12 +15,12 @@ const WalletConnect = ({ onConnect }) => {
   const connectWallet = async () => {
     try {
       await metamaskClient.connect();
-      let address = await metamaskClient.ethereumAddress; // Make sure this is correct
+      let address = await metamaskClient.ethereumAddress;
       if (address.startsWith('0x')) {
-        address = "eth|" + address.slice(2);
+        address = `eth|${address.slice(2)}`;
       }
       setWalletAddress(address);
-      onConnect(address, true, metamaskClient); // Include metamaskClient in the callback
+      onConnect(address, true, metamaskClient);
       await checkRegistration(address);
     } catch (err) {
       console.error('Error connecting wallet:', err);
@@ -31,17 +31,33 @@ const WalletConnect = ({ onConnect }) => {
   const checkRegistration = async (address) => {
     if (!address) return;
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_GATEWAY_API}/GetPublicKey`, {
-        user: address
-      });
-      setIsRegistered(!!response.data.Data);
+      const response = await getAlias(address);
+      if (response.data.Data && response.data.Data.alias) {
+        setIsRegistered(true);
+        setWalletAddress(response.data.Data.alias); // Use the alias if available
+        onConnect(response.data.Data.alias, true, metamaskClient); // Update the onConnect call with the alias
+      } else {
+        setIsRegistered(false);
+      }
     } catch (err) {
-      console.log('User is not registered', err);
-      setIsRegistered(false);
+      if (err.response && err.response.status === 404) {
+        console.log('Object not found, trying again...');
+        await checkRegistration(address); // Retry logic
+      } else {
+        console.error('Check registration error:', err);
+        setIsRegistered(false);
+      }
     }
   };
 
+  const getAlias = async (address) => {
+    return await axios.post(`${process.env.NEXT_PUBLIC_BURN_GATEWAY_PUBLIC_KEY_API}/GetObjectByKey`, {
+      objectId: `\u0000GCUP\u0000${address.replace('0x', '').replace('eth|', '')}\u0000`,
+    });
+  };
+
   const registerUser = async () => {
+    if (!walletAddress) return;
     try {
       const publicKey = await metamaskClient.getPublicKey();
       const response = await axios.post(`${process.env.NEXT_PUBLIC_GALASWAP_API}/CreateHeadlessWallet`, {
@@ -50,6 +66,7 @@ const WalletConnect = ({ onConnect }) => {
       setIsRegistered(true);
     } catch (err) {
       console.error('Error registering user:', err);
+      setIsRegistered(false);
     }
   };
 
